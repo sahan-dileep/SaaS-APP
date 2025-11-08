e2743e2b2c0591c8e8e8bffa67b40b70f203afe8
sahan-dileep <dev.shandileep@gmail.com>
2025-11-08 23:01:26 +0530

    home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welsome to my SaaS app</h1>
-      <Button>
-        Let's Get started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard id="123" name="neura the Brain Explorer" topic="Neural Network of the Brain" subject="Science" duration={45} color="#ffda6e"/>
+        <CompanionCard id="456" name="Countsy the Number Wizard" topic="Deviations & Integrais" subject="Maths" duration={30} color="#e5d0ff"/>
+        <CompanionCard id="789" name="Verba the Vocabulary builder" topic="Language" subject="English Literature" duration={30} color="#BDE7FF"/>
+      </section>
+      <section className='home-section'>
+        <CompanionsList 
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classnames="w-2/3 mag-lg:w-full"/>
+        <CTA />
+      </section>
+    </main>
   )
 }
 
---
+++ components/CTA.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const CTA = () => {
+  return (
+  <section className="cta-section">
+    <div className='cta-badge'>Start learning your way.</div>
+    <h2 className='text-3xl font-bold'>Build and Personalize Leanring Companions</h2>
+    <p>Pick a name, subject, voice & personality - and start learning through voice conversatiobs that feel neutral and fun.</p>
+    <Image src="images/cta.svg" alt="cta" width={362} height={232} />
+    <Button className='btn-primary'>
+      <Image src="/icons/plus.svg" alt="plus" width={12} height={12}/>
+    <Link href={"/companions/new"}><p>Build a New Companion</p></Link></Button>
+  </section>
+  )
+}
+
+export default CTA
